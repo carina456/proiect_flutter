8ce88687593dedc2258fb95513ef88b3a988a7a7
carina456 <andalucaci@yahoo.com>
2024-02-01 17:19:33 +0200

    tot

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      home: ContainerPage(),
+    );
+  }
+}
 class ContainerPage extends StatelessWidget {
   const ContainerPage({super.key});
 
@@ -11,51 +26,66 @@ class ContainerPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
            Row( children: [ 
-           Container(
-              height: 150,
-              width: 150,
-              child: const Center(child: Text('Make-up')),
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
-              color: Colors.pink[50],
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: Container(
+                height: 150,
+                width: 150,
+                child: const Center(child: Text('Make-up')),
+                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: Colors.pink[50],
+                ),
               ),
-            ),
-            Container(
-              height: 150,
-              width: 150,
-              child: const Center(child: Text('Nails')),
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
-              color: Colors.pink[50],
+           ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 150,
+                width: 150,
+                child: const Center(child: Text('Nails')),
+                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: Colors.pink[50],
+                ),
               ),
             ),
            ],
            ),
            Row( children: [
-            Container(
-              height: 150,
-              width: 150,
-              child: const Center(child: Text('Hair')),
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
-              color: Colors.pink[50],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 150,
+                width: 150,
+                child: const Center(child: Text('Hair')),
+                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: Colors.pink[50],
+                ),
               ),
             ),
-            Container(
-              height: 150,
-              width: 150,
-              child: const Center(child: Text('Skincare')),
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
-              color: Colors.pink[50],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 150,
+                width: 150,
+                child: const Center(child: Text('Skincare')),
+                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: Colors.pink[50],
+                ),
               ),
             ),
           ],
            ),
-          Container(
-              height: 150,
-              width: 150,
-              child: const Center(child: Text('Outfits')),
-              decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
-              color: Colors.pink[50],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+                height: 150,
+                width: 300,
+                child: const Center(child: Text('Outfits')),
+                decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30)),
+                color: Colors.pink[50],
+                ),
               ),
-            ),
+          ),
           ],
       )
       ),
